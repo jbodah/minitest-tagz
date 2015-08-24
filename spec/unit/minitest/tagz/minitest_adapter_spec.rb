807ad7c95b9ab72c9a2940163906af97727f136b
Josh Bodah <jb3689@yahoo.com>
2015-08-24 09:39:32 -0400

    add tests for Minitest::Test

--- spec/unit/minitest/tagz/minitest_adapter_spec.rb
+++ spec/unit/minitest/tagz/minitest_adapter_spec.rb
@@ -47,6 +47,22 @@ module Minitest
       end
     end
 
+    class MinitestTestExample < Minitest::Test
+      tag :login, :fast
+      def test_with_login_and_fast_tags
+        assert true
+      end
+
+      tag :login
+      def test_with_login_tag
+        assert true
+      end
+
+      def test_without_login_tag
+        assert true
+      end
+    end
+
     class MinitestSpecExampleWithSameNameButWithoutTags < Minitest::Spec
       it 'tests things with the :login tag' do
         true
@@ -55,87 +71,133 @@ module Minitest
 
     class MinitestAdapterSpec < Minitest::Spec
       after do
+        # Reset tags
         Tagz.choose_tags
       end
 
-      describe 'top level tests' do
-        describe 'with multiple tags specified' do
-          before do
-            Tagz.choose_tags(:login, :fast)
-          end
-
-          it 'runs tests with all of the tags' do
-            assert MinitestSpecExample.runnable_methods.include?('test_0002_tests things with the :login and :fast tags')
-          end
-
-          it 'does not run tests with only some of the tags' do
-            refute MinitestSpecExample.runnable_methods.include?('test_0001_tests things with the :login tag')
-          end
-
-          it 'does not run tests with none of the tags' do
-            refute MinitestSpecExample.runnable_methods.include?('test_0003_tests things with no tags')
-          end
-        end
-
-        describe 'with a single tag specified' do
+      describe 'Minitest::Test' do
+        describe 'one tag specified' do
           before do
             Tagz.choose_tags(:login)
           end
 
           it 'runs tests with that tag' do
-            assert MinitestSpecExample.runnable_methods.include?('test_0001_tests things with the :login tag')
-            assert MinitestSpecExample.runnable_methods.include?('test_0002_tests things with the :login and :fast tags')
+            assert MinitestTestExample.runnable_methods.include?('test_with_login_tag')
+            assert MinitestTestExample.runnable_methods.include?('test_with_login_and_fast_tags')
           end
 
           it 'does not run tests without that tag' do
-            refute MinitestSpecExample.runnable_methods.include?('test_0003_tests things with no tags')
+            refute MinitestTestExample.runnable_methods.include?('test_without_login_tag')
           end
         end
 
-        describe 'without a tag specified' do
-          it 'runs all tests' do
-            assert MinitestSpecExample.runnable_methods.include?('test_0001_tests things with the :login tag')
-            assert MinitestSpecExample.runnable_methods.include?('test_0002_tests things with the :login and :fast tags')
-            assert MinitestSpecExample.runnable_methods.include?('test_0003_tests things with no tags')
-          end
-        end
-      end
-
-      describe 'a suite with similarly named tests' do
-        before do
-          Tagz.choose_tags(:login)
-        end
-
-        it "does not run tests with similar names that aren't tagged" do
-          refute MinitestSpecExampleWithSameNameButWithoutTags.runnable_methods.include?('test_0001_tests things with the :login tag')
-        end
-      end
-
-      describe 'describe blocks' do
-        describe 'with a tag specified' do
+        describe 'multiple tags specified' do
           before do
-            Tagz.choose_tags(:feature)
+            Tagz.choose_tags(:login, :fast)
           end
 
-          it 'runs top level tests in the describe block' do
-            describe_block = MinitestSpecExampleWithDescribe.children.find {|c| c.name == 'a tagged describe'}
-            assert describe_block.runnable_methods.include?('test_0001_tests the first thing inside')
-            assert describe_block.runnable_methods.include?('test_0002_tests the second thing inside')
+          it 'runs tests that have all the tags' do
+            assert MinitestTestExample.runnable_methods.include?('test_with_login_and_fast_tags')
           end
 
-          it 'runs tests of nested describe blocks' do
-            describe_block = MinitestSpecExampleWithDescribe.children.find {|c| c.name == 'a tagged describe'}
-            nested_describe_block = describe_block.children.find {|c| c.name == 'a tagged describe::a nested describe'}
-            assert nested_describe_block.runnable_methods.include?('test_0001_tests things inside')
+          it 'does not run tests with some of the tags' do
+            refute MinitestTestExample.runnable_methods.include?('test_with_login_tag')
           end
 
-          it 'does not run top level tests without that tag' do
-            refute MinitestSpecExampleWithDescribe.runnable_methods.include?('test_0001_tests something outside a describe block')
+          it 'does not run tests with none of the tags' do
+            refute MinitestTestExample.runnable_methods.include?('test_without_login_tag')
+          end
+        end
+
+        describe 'no tags specified' do
+          it 'runs all of the tests' do
+            assert MinitestTestExample.runnable_methods.include?('test_with_login_tag')
+            assert MinitestTestExample.runnable_methods.include?('test_with_login_and_fast_tags')
+            assert MinitestTestExample.runnable_methods.include?('test_without_login_tag')
+          end
+        end
+      end
+
+      describe 'Minitest::Spec' do
+        describe 'top level tests' do
+          describe 'with multiple tags specified' do
+            before do
+              Tagz.choose_tags(:login, :fast)
+            end
+
+            it 'runs tests with all of the tags' do
+              assert MinitestSpecExample.runnable_methods.include?('test_0002_tests things with the :login and :fast tags')
+            end
+
+            it 'does not run tests with only some of the tags' do
+              refute MinitestSpecExample.runnable_methods.include?('test_0001_tests things with the :login tag')
+            end
+
+            it 'does not run tests with none of the tags' do
+              refute MinitestSpecExample.runnable_methods.include?('test_0003_tests things with no tags')
+            end
           end
 
-          it 'does not run tests of other describe blocks without that tag' do
-            describe_block = MinitestSpecExampleWithDescribe.children.find {|c| c.name == 'an untagged describe'}
-            refute describe_block.runnable_methods.include?('test_0001_tests the first thing inside')
+          describe 'with a single tag specified' do
+            before do
+              Tagz.choose_tags(:login)
+            end
+
+            it 'runs tests with that tag' do
+              assert MinitestSpecExample.runnable_methods.include?('test_0001_tests things with the :login tag')
+              assert MinitestSpecExample.runnable_methods.include?('test_0002_tests things with the :login and :fast tags')
+            end
+
+            it 'does not run tests without that tag' do
+              refute MinitestSpecExample.runnable_methods.include?('test_0003_tests things with no tags')
+            end
+          end
+
+          describe 'without a tag specified' do
+            it 'runs all tests' do
+              assert MinitestSpecExample.runnable_methods.include?('test_0001_tests things with the :login tag')
+              assert MinitestSpecExample.runnable_methods.include?('test_0002_tests things with the :login and :fast tags')
+              assert MinitestSpecExample.runnable_methods.include?('test_0003_tests things with no tags')
+            end
+          end
+        end
+
+        describe 'a suite with similarly named tests' do
+          before do
+            Tagz.choose_tags(:login)
+          end
+
+          it "does not run tests with similar names that aren't tagged" do
+            refute MinitestSpecExampleWithSameNameButWithoutTags.runnable_methods.include?('test_0001_tests things with the :login tag')
+          end
+        end
+
+        describe 'describe blocks' do
+          describe 'with a tag specified' do
+            before do
+              Tagz.choose_tags(:feature)
+            end
+
+            it 'runs top level tests in the describe block' do
+              describe_block = MinitestSpecExampleWithDescribe.children.find {|c| c.name == 'a tagged describe'}
+              assert describe_block.runnable_methods.include?('test_0001_tests the first thing inside')
+              assert describe_block.runnable_methods.include?('test_0002_tests the second thing inside')
+            end
+
+            it 'runs tests of nested describe blocks' do
+              describe_block = MinitestSpecExampleWithDescribe.children.find {|c| c.name == 'a tagged describe'}
+              nested_describe_block = describe_block.children.find {|c| c.name == 'a tagged describe::a nested describe'}
+              assert nested_describe_block.runnable_methods.include?('test_0001_tests things inside')
+            end
+
+            it 'does not run top level tests without that tag' do
+              refute MinitestSpecExampleWithDescribe.runnable_methods.include?('test_0001_tests something outside a describe block')
+            end
+
+            it 'does not run tests of other describe blocks without that tag' do
+              describe_block = MinitestSpecExampleWithDescribe.children.find {|c| c.name == 'an untagged describe'}
+              refute describe_block.runnable_methods.include?('test_0001_tests the first thing inside')
+            end
           end
         end
       end
