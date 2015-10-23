3775ccee02107a6a078063e76563e50eda08526b
Josh Bodah <jb3689@yahoo.com>
2015-10-23 11:33:57 -0400

    ground up refactor

--- lib/minitest/tagz.rb
+++ lib/minitest/tagz.rb
@@ -1,83 +1,240 @@
 require 'minitest/tagz/version'
-require 'minitest/tagz/minitest_adapter'
+require 'state_machine'
 
 module Minitest
   module Tagz
-    class << self
-      # Pick the tags you want to test
-      def choose_tags(*tags)
-        @chosen_tags = tags.map(&:to_sym).compact.to_set
+    module MinitestRunnerStrategy
+      class << self
+        def serialize(owner, test_name)
+          "#{owner} >> #{test_name}"
+        end
+
+        def patch
+          ::Minitest::Test.singleton_class.class_eval do
+            attr_reader :tag_map
+
+            old_runnable_methods = instance_method(:runnable_methods)
+            define_method :runnable_methods do
+              if Tagz.chosen_tags && Tagz.chosen_tags.any?
+                all_runnables = old_runnable_methods.bind(self).call
+                all_runnables.select do |r|
+                  next false unless MinitestRunnerStrategy.tag_map[MinitestRunnerStrategy.serialize(self, r)]
+                  (Tagz.chosen_tags - MinitestRunnerStrategy.tag_map[MinitestRunnerStrategy.serialize(self, r)]).empty?
+                end
+              else
+                old_runnable_methods.bind(self).call
+              end
+            end
+          end
+        end
+
+        def tag_map
+          @tag_map ||= {}
+        end
+      end
+    end
+
+    MinitestRunnerStrategy.patch
+    RunnerStrategy = MinitestRunnerStrategy
+
+    module BaseMixin
+      def tag(*tags)
+        Tagz.declare_tag_assignment(self, tags)
+      end
+    end
+
+    class TaggerFactory
+      def self.create_tagger(owner, pending_tags)
+        patchers = [MinitestPatcher]
+        patchers << ShouldaPatcher if owner.ancestors.include?(ShouldaContextLoadable)
+        Tagger.new(patchers, owner, pending_tags)
+      end
+    end
+
+    class Tagger
+      state_machine :state, initial: :awaiting_tag_declaration do
+        after_transition any => :awaiting_test_definition, do: :patch_test_definitions
+        after_transition any => :finished, do: :unpatch_test_definitions
+
+        event :tags_declared do
+          transition :awaiting_tag_declaration => :awaiting_test_definition
+        end
+
+        event :initial_test_definition_encountered do
+          transition :awaiting_test_definition => :applying_tags
+        end
+
+        event :finished_applying_tags do
+          transition :applying_tags => :finished
+        end
       end
 
-      # Create a tag on a test
-      def declare_tags(*pending_tags)
+      attr_reader :patchers, :owner, :pending_tags
+
+      def initialize(patchers, owner, pending_tags)
+        @patchers = patchers
+        @owner = owner
         @pending_tags = pending_tags
+        super()
       end
 
-      # Record the given tags with the object
-      def apply_tags(namespace, obj, pending_tags = nil)
-        if pending_tags
-          tags["#{namespace}::#{obj}"] += pending_tags
-        elsif @pending_tags
-          # TODO
-          tags["#{namespace}::#{obj}"] += @pending_tags
-          @pending_tags = nil
+      def patch_test_definitions
+        @patchers.each {|p| p.patch(self)}
+      end
+
+      def unpatch_test_definitions
+        @patchers.each(&:unpatch)
+      end
+
+      def handle_initial_test_definition
+        is_initial = awaiting_test_definition?
+        initial_test_definition_encountered if is_initial
+        res = yield
+        finished_applying_tags if is_initial
+        res
+      end
+    end
+
+    module ShouldaPatcher
+      if defined?(::Shoulda::Context::Context)
+        ::Shoulda::Context::Context.include(Tagz::BaseMixin)
+      end
+
+      # yuck
+      old_create_test_from_should_hash = Shoulda::Context::Context.instance_method(:create_test_from_should_hash)
+      Shoulda::Context::Context.class_eval do
+        define_method(:create_test_from_should_hash) do |shd|
+          if shd[:tagz]
+            test_name = old_create_test_from_should_hash.bind(self).call(shd).to_s
+            minitest_parent = self.parent
+            until minitest_parent.is_a?(Class)
+              minitest_parent = minitest_parent.parent
+            end
+            Tagz::RunnerStrategy.tag_map[Tagz::RunnerStrategy.serialize(minitest_parent, test_name)] = shd[:tagz]
+          else
+            old_create_test_from_should_hash.bind(self).call(shd)
+          end
         end
       end
 
-      def dump_tags
-        if @pending_tags
-          duped = @pending_tags.dup
-          @pending_tags = nil
-          duped
+      class << self
+        def patch(state_machine)
+          patch_context(state_machine)
+          patch_should(state_machine)
+        end
+
+        def unpatch
+          unpatch_context
+          unpatch_should
+        end
+
+        private
+
+        def patch_should(state_machine)
+          @old_should = old_should = Shoulda::Context::Context.instance_method(:should)
+          Shoulda::Context::Context.class_eval do
+            define_method(:should) do |*args, &block|
+              state_machine.handle_initial_test_definition do
+                old_should.bind(self).call(*args, &block)
+                self.shoulds.last[:tagz] = state_machine.pending_tags if state_machine.pending_tags
+              end
+            end
+          end
+        end
+
+        def patch_context(state_machine)
+          @old_context = old_context = Shoulda::Context::ClassMethods.instance_method(:context)
+          Shoulda::Context::ClassMethods.class_eval do
+            define_method(:context) do |name, *args, &block|
+              state_machine.handle_initial_test_definition do
+                old_context.bind(self).call(name, *args, &block)
+              end
+            end
+          end
+        end
+
+        def unpatch_should
+          old_should = @old_should
+          Shoulda::Context::Context.class_eval { define_method(:should, old_should) }
+        end
+
+        def unpatch_context
+          old_context = @old_context
+          Shoulda::Context::ClassMethods.class_eval { define_method(:context, old_context) }
         end
       end
+    end
 
-      def record_blanket_tags
-        if @pending_tags
-          # TODO
-          self.blanket_tags = @pending_tags
-          @pending_tags = nil
+    module MinitestPatcher
+      ::Minitest::Test.extend(Tagz::BaseMixin)
+
+      class << self
+        def patch(state_machine)
+          patch_minitest_test(state_machine)
+          patch_minitest_spec(state_machine)
+        end
+
+        def unpatch
+          unpatch_minitest_test
+          unpatch_minitest_spec
+        end
+
+        private
+
+        def patch_minitest_test(state_machine)
+          @old_method_added = old_method_added = Minitest::Test.method(:method_added)
+          Minitest::Test.class_eval do
+            define_singleton_method(:method_added) do |name|
+              if name[/^test_/]
+                state_machine.handle_initial_test_definition do
+                  Tagz::RunnerStrategy.tag_map ||= {}
+                  Tagz::RunnerStrategy.tag_map[Tagz::RunnerStrategy.serialize(self, name)] ||= []
+                  Tagz::RunnerStrategy.tag_map[Tagz::RunnerStrategy.serialize(self, name)] += state_machine.pending_tags
+                  old_method_added.call(name)
+                end
+              else
+                old_method_added.call(name)
+              end
+            end
+          end
+        end
+
+        def unpatch_minitest_test
+          Minitest::Test.define_singleton_method(:method_added, @old_method_added)
+        end
+
+        def patch_minitest_spec(state_machine)
+          @old_describe = old_describe = Minitest::Spec.method(:describe)
+          Minitest::Spec.class_eval do
+            define_singleton_method(:describe) do |*args, &block|
+              state_machine.handle_initial_test_definition do
+                old_describe.unbind.bind(self).call(*args, &block)
+              end
+            end
+          end
+        end
+
+        def unpatch_minitest_spec
+          Minitest::Spec.define_singleton_method(:describe, @old_describe)
         end
       end
+    end
 
-      def apply_blanket_tags(namespace, obj)
-        unless blanket_tags.empty?
-          tags["#{namespace}::#{obj}"] += @blanket_tags
-        end
+    class << self
+      attr_accessor :chosen_tags
+
+      # Create a master TagSet that you wish to test. You only
+      # want to run tests with tags in this set
+      # @param [Enumerable<Symbol>] tags - a list of tags you want to test
+      def choose_tags(*tags)
+        @chosen_tags = tags
       end
 
-      # Select all the testables with matching tags
-      def filter(namespace, enum)
-        enum.select {|obj| has_matching_tags?(namespace, obj)}
-      end
-
-      def reset_blanket_tags
-        @blanket_tags = []
-      end
-
-      def chosen_tags
-        @chosen_tags ||= Set.new
-      end
-
-      def tags
-        @tags ||= Hash.new([])
-      end
-
-      private
-
-      # Check if object has matching tags
-      def has_matching_tags?(namespace, obj)
-        obj_tags = tags["#{namespace}::#{obj}"] || []
-        chosen_tags.all? { |tag| obj_tags.include?(tag) }
-      end
-
-      def blanket_tags
-        @blanket_tags ||= reset_blanket_tags
-      end
-
-      def blanket_tags=(arg)
-        @blanket_tags = arg
+      def declare_tag_assignment(owner, pending_tags)
+        tag_machine = TaggerFactory.create_tagger(owner, pending_tags)
+        tag_machine.tags_declared
+        # TODO add debugging tip about this
+        tag_machine
       end
     end
   end
--- spec/unit/minitest/tagz/minitest_adapter_spec.rb
+++ spec/unit/minitest/tagz/minitest_adapter_spec.rb
@@ -5,15 +5,18 @@ module Minitest
     class MinitestSpecExample < Minitest::Spec
       tag :login
       it 'tests things with the :login tag' do
+        skip
         true
       end
 
       tag :login, :fast
       it 'tests things with the :login and :fast tags' do
+        skip
         true
       end
 
       it 'tests things with no tags' do
+        skip
         true
       end
     end
@@ -22,15 +25,18 @@ module Minitest
       tag :feature
       describe 'a tagged describe' do
         it 'tests the first thing inside' do
+          skip
           true
         end
 
         it 'tests the second thing inside' do
+          skip
           true
         end
 
         describe 'a nested describe' do
           it 'tests things inside' do
+            skip
             true
           end
         end
@@ -38,11 +44,13 @@ module Minitest
 
       describe 'an untagged describe' do
         it 'tests the first thing inside' do
+          skip
           true
         end
       end
 
       it 'tests something outside a describe block' do
+        skip
         true
       end
     end
@@ -50,21 +58,25 @@ module Minitest
     class MinitestTestExample < Minitest::Test
       tag :login, :fast
       def test_with_login_and_fast_tags
+        skip
         assert true
       end
 
       tag :login
       def test_with_login_tag
+        skip
         assert true
       end
 
       def test_without_login_tag
+        skip
         assert true
       end
     end
 
     class MinitestSpecExampleWithSameNameButWithoutTags < Minitest::Spec
       it 'tests things with the :login tag' do
+        skip
         true
       end
     end
@@ -82,12 +94,15 @@ module Minitest
           end
 
           it 'runs tests with that tag' do
-            assert MinitestTestExample.runnable_methods.include?('test_with_login_tag')
-            assert MinitestTestExample.runnable_methods.include?('test_with_login_and_fast_tags')
+            assert MinitestTestExample.runnable_methods.include?('test_with_login_tag'),
+              "expected tests with tag to be run"
+            assert MinitestTestExample.runnable_methods.include?('test_with_login_and_fast_tags'),
+              "expected tests with tag and more to be run"
           end
 
           it 'does not run tests without that tag' do
-            refute MinitestTestExample.runnable_methods.include?('test_without_login_tag')
+            refute MinitestTestExample.runnable_methods.include?('test_without_login_tag'),
+              "didn't expect test without tag to be run"
           end
         end
 
@@ -97,23 +112,26 @@ module Minitest
           end
 
           it 'runs tests that have all the tags' do
-            assert MinitestTestExample.runnable_methods.include?('test_with_login_and_fast_tags')
+            assert MinitestTestExample.runnable_methods.include?('test_with_login_and_fast_tags'),
+              "expected test with all tags to be run"
           end
 
           it 'does not run tests with some of the tags' do
-            refute MinitestTestExample.runnable_methods.include?('test_with_login_tag')
+            refute MinitestTestExample.runnable_methods.include?('test_with_login_tag'),
+              "didn't expect test with only some matching tags to be run"
           end
 
           it 'does not run tests with none of the tags' do
-            refute MinitestTestExample.runnable_methods.include?('test_without_login_tag')
+            refute MinitestTestExample.runnable_methods.include?('test_without_login_tag'),
+              "didn't expect test with no matching tags to be run"
           end
         end
 
         describe 'no tags specified' do
           it 'runs all of the tests' do
-            assert MinitestTestExample.runnable_methods.include?('test_with_login_tag')
-            assert MinitestTestExample.runnable_methods.include?('test_with_login_and_fast_tags')
-            assert MinitestTestExample.runnable_methods.include?('test_without_login_tag')
+            assert MinitestTestExample.runnable_methods.include?('test_with_login_tag'), 'expected all tests to be run'
+            assert MinitestTestExample.runnable_methods.include?('test_with_login_and_fast_tags'), 'expected all tests to be run'
+            assert MinitestTestExample.runnable_methods.include?('test_without_login_tag'), 'expected all tests to be run'
           end
         end
       end
@@ -134,7 +152,8 @@ module Minitest
             end
 
             it 'does not run tests with none of the tags' do
-              refute MinitestSpecExample.runnable_methods.include?('test_0003_tests things with no tags')
+              refute MinitestSpecExample.runnable_methods.include?('test_0003_tests things with no tags'),
+                "didn't expect test with none of the tags to be run"
             end
           end
 
@@ -149,7 +168,8 @@ module Minitest
             end
 
             it 'does not run tests without that tag' do
-              refute MinitestSpecExample.runnable_methods.include?('test_0003_tests things with no tags')
+              refute MinitestSpecExample.runnable_methods.include?('test_0003_tests things with no tags'),
+                "shouldn't run spec tests without that tag"
             end
           end
 
@@ -168,7 +188,8 @@ module Minitest
           end
 
           it "does not run tests with similar names that aren't tagged" do
-            refute MinitestSpecExampleWithSameNameButWithoutTags.runnable_methods.include?('test_0001_tests things with the :login tag')
+            refute MinitestSpecExampleWithSameNameButWithoutTags.runnable_methods.include?('test_0001_tests things with the :login tag'),
+              "it shouldn't run spec tests with similar names that aren't tagged"
           end
         end
 
@@ -181,7 +202,8 @@ module Minitest
             it 'runs top level tests in the describe block' do
               describe_block = MinitestSpecExampleWithDescribe.children.find {|c| c.name == 'a tagged describe'}
               assert describe_block.runnable_methods.include?('test_0001_tests the first thing inside')
-              assert describe_block.runnable_methods.include?('test_0002_tests the second thing inside')
+              assert describe_block.runnable_methods.include?('test_0002_tests the second thing inside'),
+                "expected top level test to be run"
             end
 
             it 'runs tests of nested describe blocks' do
@@ -191,12 +213,14 @@ module Minitest
             end
 
             it 'does not run top level tests without that tag' do
-              refute MinitestSpecExampleWithDescribe.runnable_methods.include?('test_0001_tests something outside a describe block')
+              refute MinitestSpecExampleWithDescribe.runnable_methods.include?('test_0001_tests something outside a describe block'),
+                "didn't expect top level test without that tag to be run"
             end
 
             it 'does not run tests of other describe blocks without that tag' do
               describe_block = MinitestSpecExampleWithDescribe.children.find {|c| c.name == 'an untagged describe'}
-              refute describe_block.runnable_methods.include?('test_0001_tests the first thing inside')
+              refute describe_block.runnable_methods.include?('test_0001_tests the first thing inside'),
+                "dind't expect test from other describe block to be run"
             end
           end
         end
@@ -206,24 +230,28 @@ module Minitest
     class ShouldaContextSpec < Minitest::Spec
       include ShouldaContextLoadable
 
+      before do
+        @serialized = Minitest::Tagz::RunnerStrategy.serialize(ShouldaContextSpec, name)
+      end
+
       context 'tags on should blocks' do
         should 'not tag this test' do
-          refute Tagz.tags.include?(self.class.name + '::' + name)
+          assert_equal nil, Minitest::Tagz::RunnerStrategy.tag_map[@serialized]
         end
 
         tag :shoulda_tag
         should 'tag this test' do
-          assert Tagz.tags.include?(self.class.name + '::' + name)
+          assert_equal [:shoulda_tag], Minitest::Tagz::RunnerStrategy.tag_map[@serialized]
         end
 
         should 'not tag this test either' do
-          refute Tagz.tags.include?(self.class.name + '::' + name)
+          assert_equal nil, Minitest::Tagz::RunnerStrategy.tag_map[@serialized]
         end
 
         context 'tags on nested should blocks' do
           tag :another_should_tag
           should 'tag this test' do
-            assert Tagz.tags.include?(self.class.name + '::' + name)
+            assert_equal [:another_should_tag], Minitest::Tagz::RunnerStrategy.tag_map[@serialized]
           end
         end
       end
@@ -231,13 +259,11 @@ module Minitest
       tag :context_tag
       context 'tags on context blocks' do
         should 'tag this test' do
-          skip
-          assert Tagz.tags.include?(self.class.name + '::' + name)
+          assert_equal [:context_tag], Minitest::Tagz::RunnerStrategy.tag_map[@serialized]
         end
 
         should 'also tag this test' do
-          skip
-          assert Tagz.tags.include?(self.class.name + '::' + name)
+          assert_equal [:context_tag], Minitest::Tagz::RunnerStrategy.tag_map[@serialized]
         end
       end
 
@@ -245,13 +271,11 @@ module Minitest
       context 'tags on nested context blocks' do
         context 'a nested context block' do
           should 'tag this test' do
-            skip
-            assert Tagz.tags.include?(self.class.name + '::' + name)
+            assert_equal [:nested_context_tag], Minitest::Tagz::RunnerStrategy.tag_map[@serialized]
           end
 
           should 'also tag this test' do
-            skip
-            assert Tagz.tags.include?(self.class.name + '::' + name)
+            assert_equal [:nested_context_tag], Minitest::Tagz::RunnerStrategy.tag_map[@serialized]
           end
         end
       end
